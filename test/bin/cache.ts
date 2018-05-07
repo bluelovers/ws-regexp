7aee1528797bb4bfbea7ca56c335490c2ea5ed35
bluelovers <codelovers@users.sourceforge.net>
2018-05-07 13:40:20 +0800

    .

--- test/bin/cache.ts
+++ test/bin/cache.ts
@@ -8,9 +8,55 @@ import * as path from 'path';
 import * as globby from 'globby';
 import * as fs from 'fs-extra';
 
+export const REGEXPP_PATH = path.dirname(require.resolve('regexpp2/package.json')) as string;
+
+export const REGEXPP_PATH_DATA = path.join(REGEXPP_PATH, 'src/unicode/property-data') as string;
+
 export const XREGEXP_PATH = path.dirname(require.resolve('xregexp/package.json')) as string;
 
 export const XREGEXP_PATH_DATA = path.join(XREGEXP_PATH, 'tools/output') as string;
+
+const PropertyData = require_default(REGEXPP_PATH_DATA).PropertyData;
+
+(async () =>
+{
+
+	let output = [];
+	let def = [
+		'$LONE',
+		'General_Category',
+		'Script',
+	];
+
+	def.forEach(function (key)
+	{
+
+		//console.log(PropertyData[key]);
+
+		let d = [...PropertyData[key]]
+			.reduce(function (a, b: string)
+			{
+				a[b] = false;
+
+				return a;
+			}, {})
+		;
+
+		let s = JSON.stringify(d, null, "\t");
+
+		output.push(`export const ${key} = ${s};`);
+
+	});
+
+	output.push(`export default { ${def.join(', ')} }`);
+
+	let name = 'property-data';
+
+	let file = path.join(__dirname, '../../', 'lib/pattern/cache', name + '.ts');
+
+	fs.outputFile(file, output.join('\n\n'));
+
+})();
 
 globby([
 	'*.js',
