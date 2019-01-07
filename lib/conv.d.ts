9757c373fe40137b241ea2059f13b705c1d6de48
bluelovers <codelovers@users.sourceforge.net>
2019-01-08 05:43:48 +0800

    .

--- lib/conv.d.ts
+++ lib/conv.d.ts
@@ -1,6 +1,4 @@
-/**
- * Created by user on 2018/5/5/005.
- */
+/// <reference path="../../cjk-convert/cjk-conv.d.ts" />
 import cjkConv from 'cjk-conv';
 export declare function _word_zh_core(search: string, skip?: string, zhTable?: typeof cjkConv.zhTable.auto): string;
 export declare function _word_zh_core2(search: string, skip?: string, zhTable?: typeof cjkConv.zhTable.auto): string;
