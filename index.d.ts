e88c2717d8aeb4f2016e597b6cc5a533078bc121
bluelovers <codelovers@users.sourceforge.net>
2019-01-29 12:37:10 +0800

    try avoid webpack + terser bug

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,6 @@
+/**
+ * Created by user on 2018/1/31/031.
+ */
 import { ParserEventEmitterEvent, IParserEventEmitterListener } from 'regexp-parser-event';
 import { IAstToStringOptions } from 'regexp-parser-literal';
 import _support from 'regexp-support';
@@ -5,14 +8,23 @@ import RegexpHelper = require('regexp-helper');
 export declare type IOptions = {
     skip?: string;
     disableZh?: boolean;
+    /**
+     * disableLocalRange only work when disableZh is true
+     */
     disableLocalRange?: boolean;
     allowLocalRangeAutoZh?: boolean;
     flags?: string;
+    /**
+     * allow str is /a/g
+     */
     parseRegularExpressionString?: boolean;
     on?: {
         [k in keyof typeof ParserEventEmitterEvent]?: IParserEventEmitterListener<any>;
     };
     greedyTable?: boolean;
+    /**
+     * allow set `CjkConv.zhTable.auto`
+     */
     zhTable?: (char: string) => string[];
 } & IAstToStringOptions;
 export declare const defaultOptions: IOptions;
@@ -26,16 +38,44 @@ export declare class zhRegExp extends RegExp {
     sticky: boolean;
     unicode: boolean;
     lastIndex: number;
+    /**
+     * The non-standard leftContext property is a static and read-only property of regular expressions that contains the substring preceding the most recent match. RegExp.$` is an alias for this property.
+     *
+     * @alias $`
+     */
     static readonly leftContext: string;
+    /**
+     * The non-standard rightContext property is a static and read-only property of regular expressions that contains the substring following the most recent match. RegExp.$' is an alias for this property.
+     *
+     * @alias $'
+     */
     static readonly rightContext: string;
+    /**
+     * The non-standard lastParen property is a static and read-only property of regular expressions that contains the last parenthesized substring match, if any. RegExp.$+ is an alias for this property.
+     *
+     * @alias $+
+     */
     static readonly lastParen: string;
+    /**
+     * The non-standard lastMatch property is a static and read-only property of regular expressions that contains the last matched characters. RegExp.$& is an alias for this property.
+     *
+     * @alias $&
+     */
     static readonly lastMatch: string;
+    /**
+     * The non-standard input property is a static property of regular expressions that contains the string against which a regular expression is matched. RegExp.$_ is an alias for this property.
+     *
+     * @alias $_
+     */
     static readonly input: string;
     constructor(str: string | RegExp, flags?: string, options?: IOptions | string, ...argv: any[]);
     constructor(str: string | RegExp, options?: IOptions, ...argv: any[]);
     static create<T = zhRegExp>(str: string | RegExp, flags?: string, options?: IOptions | string): T;
     static create<T = zhRegExp>(str: string | RegExp, options?: IOptions): T;
     getStatic<T = typeof zhRegExp>(): T;
+    /**
+     * @todo
+     */
     toRegularExpressionString(): string;
     static parseRegularExpressionString(str: string): {
         source: string;
