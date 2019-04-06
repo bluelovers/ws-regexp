b2bfe085da3e0d5674fc933dbe98acbddd1185f2
bluelovers <codelovers@users.sourceforge.net>
2019-04-07 06:03:00 +0800

    [breaking change] remove old code

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -3,9 +3,9 @@
  *
  * 已廢棄 僅用於舊版相容
  */
-export * from './v1';
+import { _word_zh_core } from './conv';
 export declare function _word_zh(search: string, ret: any, flags?: any, skip?: string): any;
 export declare function _word_zh(search: RegExp, ret: any, flags?: any, skip?: string): any;
-export declare function _word_zh_core(search: string, skip?: string): string;
+export { _word_zh_core };
 declare const _default: typeof import(".");
 export default _default;
--- lib/index.ts
+++ lib/index.ts
@@ -7,7 +7,8 @@
 import cjkConv from 'cjk-conv';
 import zhTable, { IOptions } from 'cjk-conv/lib/zh/table/index';
 import { zhRegExp } from '..';
-export * from './v1';
+import { _word_zh_core } from './conv';
+//export * from './v1';
 
 export function _word_zh(search: string, ret, flags?, skip?: string)
 export function _word_zh(search: RegExp, ret, flags?, skip?: string)
@@ -33,20 +34,7 @@ export function _word_zh(search, ret, flags = 'ig', skip?: string)
 	return [s, ret, flags];
 }
 
-export function _word_zh_core(search: string, skip?: string)
-{
-	return search.replace(/[\u4E00-\u9FFF\u{20000}-\u{2FA1F}のと]/ug, function (char)
-	{
-		if (skip && skip.indexOf(char) != -1)
-		{
-			return char;
-		}
-
-		let a = zhTable.auto(char);
-
-		return a.length > 1 ? '[' + a.join('') + ']' : a[0];
-	});
-}
+export { _word_zh_core }
 
 //export type valueof<T> = T[keyof T];
 
