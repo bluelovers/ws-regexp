dfa20cf0325bb03db5e8325e77090beb55a01c8f
James Moon <mooniker@gmail.com>
2019-09-15 14:12:06 -0400

    Add Unicode block references for Hangul, CJK/Latin

--- constants.js
+++ constants.js
@@ -2,5 +2,14 @@ module.exports = {
   HANGUL_SYLLABLE_BLOCK: {
     START: 0xac00,
     END: 0xd7af
-  }
+  },
+  HANGUL_UNICODE_BLOCKS: {
+    SYLLABLES: [0xac00, 0xd7af],
+    JAMO: [0x1100, 0x11ff],
+    COMPATIBILITY_JAMO: [0x3130, 0x318f],
+    EXTENDED_A: [0xa960, 0xa97f],
+    EXTENDED_B: [0xd7b0, 0xd7ff]
+  },
+  BASIC_LATIN: [0x0000, 0x007f],
+  CJK_UNIFIED_IDEOGRAPHS: [0x4e00, 0x9fff]
 };
