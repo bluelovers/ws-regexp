cdc0fa3c7cdbb60b6db603b582979d55392feaf4
James Moon <mooniker@gmail.com>
2019-09-15 14:24:39 -0400

    Add jamo transliteration function, tests

---
+++ translit.jamo.js
@@ -0,0 +1,35 @@
+const isHangul = require("./translit.isHangul");
+
+const romanize = require("./jamo.rr");
+
+const translitJamo = (input, position = 0, prev, method = "rr") => {
+  // if given input is hangul, kick off romanization lookup
+  if (isHangul(input)) {
+    return lookup(romanize[input]);
+  }
+
+  // else if string, assume romanization completed
+  if (typeof input === "string") {
+    return input;
+  }
+
+  // if input from lookup is array
+  // transliteration depends on jamo position
+  if (Array.isArray(input)) {
+    const i = position && position > 0 ? 1 : 0;
+    return lookup(input[i]);
+  }
+
+  if (typeof input !== "object") {
+    throw new Error("Nonobject should not get here.", input, typeof input);
+  }
+
+  // if provided jamo lookup object has key for requested method
+  return lookup(input[method] || input[prev] || input["default"]);
+
+  function lookup(key) {
+    return translitJamo(key, position, prev, method);
+  }
+};
+
+module.exports = translitJamo;
---
+++ translit.jamo.test.js
@@ -0,0 +1,58 @@
+const translitJamo = require("./translit.jamo");
+
+describe("translitJamo", () => {
+  test("should transliterate j as j", () => {
+    expect(translitJamo("j")).toBe("j");
+  });
+
+  test("should transliterate g as g", () => {
+    expect(translitJamo("g")).toBe("g");
+  });
+
+  test("should transliterate b as b", () => {
+    expect(translitJamo("b")).toBe("b");
+  });
+
+  test("should transliterate [d,t] as d", () => {
+    // expect(translitJamo(["d", "t"])).toBe("d");
+    expect(translitJamo(["d", "t"])).toBe("d");
+  });
+
+  test("should transliterate [d,t] as d", () => {
+    expect(translitJamo(["d", "t"], 1)).toBe("t");
+  });
+
+  test("should transliterate ㅈ as j", () => {
+    expect(translitJamo("ㅈ")).toBe("j");
+  });
+
+  test("should transliterate ㅈ as j", () => {
+    expect(translitJamo("ㅈ")).toBe("j");
+  });
+
+  describe("ㄷ", () => {
+    test("should transliterate ㄷ as initial as d", () => {
+      expect(translitJamo("ㄷ")).toBe("d");
+    });
+
+    test("should transliterate ㄷ as final as t", () => {
+      expect(translitJamo("ㄷ", 2)).toBe("t");
+    });
+
+    test("should transliterate final ㄷ followed by initial ㄴ as d", () => {
+      expect(translitJamo("ㄷ", 2, "ㄴ")).toBe("nn");
+    });
+
+    test("should transliterate final ㄷ followed by initial ㄴ as d", () => {
+      expect(translitJamo("ㄷ", 2, "ㄹ")).toBe("nn");
+    });
+
+    test("should transliterate final ㄷ followed by initial ㅁ as d", () => {
+      expect(translitJamo("ㄷ", 2, "ㅁ")).toBe("nm");
+    });
+
+    test("should transliterate final ㄷ followed by initial ㅌ as d", () => {
+      expect(translitJamo("ㄷ", 2, "ㅌ")).toBe("t-t");
+    });
+  });
+});
