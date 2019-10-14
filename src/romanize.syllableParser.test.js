90fff152c360acb1af27f73c249da3217c0472a7
James Moon <mooniker@gmail.com>
2019-10-14 00:27:41 -0400

    Add syllable parser tests

---
+++ src/romanize.syllableParser.test.js
@@ -0,0 +1,24 @@
+const { syllableParser } = require("./romanize");
+
+describe("syllableParser", () => {
+  const parseSyllable = syllableParser({});
+
+  test("should return a function", () => {
+    expect(parseSyllable).toBeInstanceOf(Function);
+  });
+
+  const hunminjeongeum = [
+    [0x1112, 0x116e, 0x11ab],
+    [0x1106, 0x1175, 0x11ab],
+    [0x110c, 0x1165, 0x11bc],
+    [0x110b, 0x1173, 0x11b7]
+  ];
+  test("should parse 훈민정음 to [[h,u,n],[m,i,n],[j,eo,ng-],[,eu,m]]", () => {
+    expect(hunminjeongeum.map(parseSyllable)).toStrictEqual([
+      ["h", "u", "n"],
+      ["m", "i", "n"],
+      ["j", "eo", "ng-"],
+      ["", "eu", "m"]
+    ]);
+  });
+});
