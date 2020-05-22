ea84501d0f6fe4879940922323eb9686fc37ca68
bluelovers <codelovers@users.sourceforge.net>
2020-05-23 00:13:21 +0800

    build: @lazy-cjk/zh-convert-table

---
+++ packages/@lazy-cjk/zh-convert-table/table.d.ts
@@ -0,0 +1,5 @@
+import { ITable } from './lib/types';
+export declare const table_cn2tw: ITable;
+export declare const table_tw2cn: ITable;
+declare const _default: typeof import("./table");
+export default _default;
---
+++ packages/@lazy-cjk/zh-convert-table/wikipedia.d.ts
@@ -0,0 +1,7 @@
+import { ITable } from './lib/types';
+export declare const wiki_s2t: ITable;
+export declare const wiki_t2s: ITable;
+export declare const wiki_s2t_v2: ITable;
+export declare const wiki_t2s_v2: ITable;
+declare const _default: typeof import("./wikipedia");
+export default _default;
