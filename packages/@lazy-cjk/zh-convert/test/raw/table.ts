d6769bbe4bd035c20633bf017b87655b1b69fe92
bluelovers <codelovers@users.sourceforge.net>
2020-06-01 15:33:49 +0800

    .

--- packages/@lazy-cjk/zh-convert/test/raw/table.ts
+++ packages/@lazy-cjk/zh-convert/test/raw/table.ts
@@ -1,5 +1,5 @@
 import { ITable } from '../../lib/types';
-import { table_cn2tw as _table_cn2tw, table_tw2cn as _table_tw2cn } from '@lazy-cjk/zh-convert-table';
+import { table_cn2tw as _table_cn2tw, table_tw2cn as _table_tw2cn } from '@lazy-cjk/zh-convert-table/table';
 import { table_cn2tw_plus, table_tw2cn_plus } from '@lazy-cjk/zh-convert-table/table_plus';
 
 export let table_cn2tw: ITable = Object.assign({}, _table_cn2tw, table_cn2tw_plus);
