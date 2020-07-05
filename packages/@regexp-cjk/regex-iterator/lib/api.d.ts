41cec539c3fee6c2578b5a2e46a25a981e5c5657
bluelovers <codelovers@users.sourceforge.net>
2020-07-05 17:43:10 +0800

    build: update api index

---
+++ packages/@regexp-cjk/regex-iterator/lib/api.d.ts
@@ -0,0 +1,6 @@
+import each from './api/each';
+import map from './api/map';
+import reduce from './api/reduce';
+export { each };
+export { map };
+export { reduce };
---
+++ packages/@regexp-cjk/regex-iterator/lib/api.ts
@@ -0,0 +1,8 @@
+
+import each from './api/each';
+import map from './api/map';
+import reduce from './api/reduce';
+
+export { each }
+export { map }
+export { reduce }
