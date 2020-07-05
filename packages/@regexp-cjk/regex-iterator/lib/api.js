478d2b4bf76d8520d1c9b04e9116bc39e1dc1fe0
bluelovers <codelovers@users.sourceforge.net>
2020-07-05 20:04:10 +0800

    build: update api index

--- packages/@regexp-cjk/regex-iterator/lib/api.js
+++ packages/@regexp-cjk/regex-iterator/lib/api.js
@@ -3,11 +3,13 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.reduce = exports.map = exports.each = void 0;
+exports.replaceEach = exports.reduce = exports.map = exports.each = void 0;
 const each_1 = __importDefault(require("./api/each"));
 exports.each = each_1.default;
 const map_1 = __importDefault(require("./api/map"));
 exports.map = map_1.default;
 const reduce_1 = __importDefault(require("./api/reduce"));
 exports.reduce = reduce_1.default;
+const replaceEach_1 = __importDefault(require("./api/replaceEach"));
+exports.replaceEach = replaceEach_1.default;
 //# sourceMappingURL=api.js.map
--- packages/@regexp-cjk/regex-iterator/lib/api.ts
+++ packages/@regexp-cjk/regex-iterator/lib/api.ts
@@ -2,7 +2,9 @@
 import each from './api/each';
 import map from './api/map';
 import reduce from './api/reduce';
+import replaceEach from './api/replaceEach';
 
 export { each }
 export { map }
 export { reduce }
+export { replaceEach }
