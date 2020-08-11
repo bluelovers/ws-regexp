15c53d73b2214e1018db7da3193955570710b0f8
bluelovers <codelovers@users.sourceforge.net>
2020-08-11 15:24:41 +0800

    build: update file

--- packages/@lazy-cjk/zh-convert-table/table.d.ts
+++ packages/@lazy-cjk/zh-convert-table/table.d.ts
@@ -1,5 +1,5 @@
 import { ITable } from './lib/types';
 export declare const table_cn2tw: ITable;
 export declare const table_tw2cn: ITable;
-declare const _default: typeof import("./table");
+declare const _default: typeof import("@lazy-cjk/zh-convert-table/table");
 export default _default;
