900354cf736ccaca700dfde7194c431139b64610
bluelovers <codelovers@users.sourceforge.net>
2022-09-19 08:24:09 +0800

    build: update api index

--- packages/@regexp-cjk/regex-iterator/lib/api.js
+++ packages/@regexp-cjk/regex-iterator/lib/api.js
@@ -1,14 +1,15 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.splitLimit = exports.replaceEach = exports.reduce = exports.map = exports.each = void 0;
-const each_1 = require("./api/each");
-Object.defineProperty(exports, "each", { enumerable: true, get: function () { return each_1.each; } });
-const map_1 = require("./api/map");
-Object.defineProperty(exports, "map", { enumerable: true, get: function () { return map_1.map; } });
-const reduce_1 = require("./api/reduce");
-Object.defineProperty(exports, "reduce", { enumerable: true, get: function () { return reduce_1.reduce; } });
-const replaceEach_1 = require("./api/replaceEach");
-Object.defineProperty(exports, "replaceEach", { enumerable: true, get: function () { return replaceEach_1.replaceEach; } });
-const splitLimit_1 = require("./api/splitLimit");
-Object.defineProperty(exports, "splitLimit", { enumerable: true, get: function () { return splitLimit_1.splitLimit; } });
+const tslib_1 = require("tslib");
+const each_1 = tslib_1.__importDefault(require("./api/each"));
+exports.each = each_1.default;
+const map_1 = tslib_1.__importDefault(require("./api/map"));
+exports.map = map_1.default;
+const reduce_1 = tslib_1.__importDefault(require("./api/reduce"));
+exports.reduce = reduce_1.default;
+const replaceEach_1 = tslib_1.__importDefault(require("./api/replaceEach"));
+exports.replaceEach = replaceEach_1.default;
+const splitLimit_1 = tslib_1.__importDefault(require("./api/splitLimit"));
+exports.splitLimit = splitLimit_1.default;
 //# sourceMappingURL=api.js.map
